dbd79f93986a28416c8f1cdecb599dec58014760
Adrian <adrian@pop-os.localdomain>
2022-11-26 13:41:46 -0700

    Uploading Template

---
+++ lab37-2.c
@@ -0,0 +1,20 @@
+/** lab37-2.c
+* ===========================================================
+* Name: FIRST LAST, DATE
+* Section: SECTION
+* Documentation: YOUR DOC STATEMENT
+* ===========================================================  */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include "lab37functs.h"
+
+int main() {
+      
+    // YOU CAN INCLUDE TEST CODE HERE
+    // TEST CODE IS NOT GRADED
+   
+    return 0;
+}
---
+++ lab37functs.h
@@ -0,0 +1,15 @@
+/** lab37.h
+* ===========================================================
+* Name: FIRST LAST, DATE
+* Section: SECTION
+* ===========================================================
+*/
+
+#ifndef LAB37_H
+#define LAB37_H
+
+void print_bits(void* ptr, int num_bytes);
+
+// YOUR FUNCTIONS GO HERE
+
+#endif
