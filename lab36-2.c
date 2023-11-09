28f0efdb54bff96ffcf4465622bafe0d22bf9acc
troywein <45960380+troywein@users.noreply.github.com>
2023-11-09 16:43:55 -0700

    Update and rename lab37-2.c to lab36-2.c

--- lab36-2.c
+++ lab36-2.c
@@ -1,4 +1,4 @@
-/** lab37-2.c
+/** lab36-2.c
 * ===========================================================
 * Name: FIRST LAST, DATE
 * Section: SECTION
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#include "lab37functs.h"
+#include "lab36functs.h"
 
 int main() {
       
